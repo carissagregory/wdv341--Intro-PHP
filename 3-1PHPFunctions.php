eb996515a5b508d24e251ec644be5cc90434aa5b
Carissa Gregory <carissagregory@Carissas-MacBook-Air.local>
2024-09-16 17:17:21 -0500

    added heading

--- 3-1PHPFunctions.php
+++ 3-1PHPFunctions.php
@@ -55,9 +55,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>3-1 PHP Functions</title>
 </head>
 <body>
-    
+<h1>3-1 PHP Functions</h1>
 </body>
 </html>
