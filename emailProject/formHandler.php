d4c10566745bbc9c95de1f366a84b77b9a40d3e4
Carissa Gregory <carissagregory@Carissas-MacBook-Air.local>
2024-12-09 23:05:21 -0600

    added google reCaptcha(omitted keys on repo) and added failed email and redirect upon failure

--- emailProject/formHandler.php
+++ emailProject/formHandler.php
@@ -7,43 +7,61 @@
 
 -->
 <?php
+    if ($responseKeys["success"] && $responseKeys["score"] > 0.5) {
+    // If reCAPTCHA is valid, process the form data
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $userEnteredEmail = $_POST["email"];
+        $to = "contact@coconutlavender.com"; 
+        $subject = $_POST["contactReason"];
+        $message = 
+            "Contact form was submitted on: " . date("m-d-Y") . "\n" . 
+            "You are being contacted by: " . $firstName . " " . $lastName . "\n" . 
+            "Email: " . $userEnteredEmail . "\n" . 
+            "They are contacting you because: " . $subject . "\n\n" . 
+            "They left the message:\n\n" . $_POST["message"];
 
-    //Email to forwarding email
-    $firstName = $_POST["firstName"];
-    $lastName = $_POST["lastName"];
-    $userEnteredEmail = $_POST["email"];
-    $to = "contact@coconutlavender.com"; 
-    $subject = $_POST["contactReason"];
-    $message = 
+        $headers = "From: contact@coconutlavender.com" . "\r\n" . 
+                "Reply-To: " . $userEnteredEmail . "\r\n";
 
-        "Contact form was submitted on: " . date("m-d-Y") . "\n" . 
-        "You are being contacted by: " . $firstName . " " . $lastName . "\n" . 
-        "Email: " . $userEnteredEmail . "\n" . 
-        "They are contacting you because: " . $subject . "\n\n" . 
-        "They left the message:\n\n" . $_POST["message"]
+        mail($to, $subject, $message, $headers, '-f contact@coconutlavender.com');
 
-    ;
-    $headers = "From: contact@coconutlavender.com" . "\r\n" . 
-                "Reply-To:" . $userEnteredEmail . "\r\n";
+        $userConfirmationMessage = 
+            "Hello " . $firstName . " " . $lastName . ",\n\n" . 
+            "Thank you for contacting me! Once I receive your email, I will get back to you shortly.\n\n" . 
+            "Below is the information you submitted through my contact form:\n\n" . 
+            "Date Submitted: " . date("m-d-Y") . "\n" . 
+            "Reason for contact: " . $subject . "\n" . 
+            "Message you left:\n\n" . $_POST["message"] . "\n\n" . 
+            "Thank you,\n Carissa";
 
-    mail($to, $subject, $message, $headers, '-f contact@coconutlavender.com');
+        $userHeaders = "From: contact@coconutlavender.com" . "\r\n" . 
+                    "Reply-To: contact@coconutlavender.com";
 
+        // Send confirmation email to the user
+        mail($userEnteredEmail, $subject, $userConfirmationMessage, $userHeaders, '-f contact@coconutlavender.com');
+        //echo $response;
+    } else {
+        // If reCAPTCHA fails, send non-successful email message to contact@coconutlavender.com and redirect to form
+        $firstName = $_POST["firstName"];
+        $lastName = $_POST["lastName"];
+        $userEnteredEmail = $_POST["email"];
+        $to = "contact@coconutlavender.com"; 
+        $subject = $_POST["contactReason"];
+        $message = 
+            "ReCaptcha prevented an email submission" . "\n" . 
+            "Date: " . date("m-d-Y") . "\n" . 
+            "By: " . $firstName . " " . $lastName . "\n" . 
+            "Email: " . $userEnteredEmail . "\n" . 
+            "They are contacting you because: " . $subject . "\n\n" . 
+            "They left the message:\n\n" . $_POST["message"];
 
-    //Email to user    
-    $userConfirmationMessage =
+        $headers = "From: contact@coconutlavender.com" . "\r\n" . 
+                "Reply-To: " . $userEnteredEmail . "\r\n";
 
-        "Hello " . $firstName . " " . $lastName . "," . "\n\n" . 
-        "Thank you for contacting me! Once I recieve your email I will get back to you shortly." . "\n\n" . 
-        "Below is the information you submitted through my contact form:" . "\n\n" . 
-        "Date Submitted: " . date("m-d-Y") . "\n" . 
-        "Reason for contact:" . $subject . "\n" . 
-        "Message you left:\n\n" . $_POST["message"] . "\n" . 
-        "Thank you,\n Carissa"
-    ;
-    $userHeaders = "From: contact@coconutlavender.com" . "\r\n" . 
-                  "Reply-To: contact@coconutlavender.com";
-
-    mail($userEnteredEmail, $subject, $userConfirmationMessage, $userHeaders, '-f contact@coconutlavender.com')
+        mail($to, $subject, $message, $headers, '-f contact@coconutlavender.com');
+        header("Location: contactForm.html");
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
